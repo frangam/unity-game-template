ddd00749850e366ea5df6f2f23a26fb8006c8afe
Fran García <fmgarmo@gmail.com>
2014-12-02 21:55:41 +0100

    added persistentSingleton pattern

---
+++ Assets/GameTemplate/Scripts/Utils/Unity/Patterns/PersistentSingleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Singleton pattern for object we want to exist in all of the Scenes
+/// </summary>
+public class PersistentSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+	protected static T instance;
+	
+	/// <summary>
+	/// Returns instance of this class
+	/// </summary>
+	/// <value>
+	/// The instance.
+	/// </value>
+	public static T Instance {
+		get {
+			if (FindObjectsOfType(typeof(T)).Length > 1 ){
+				Debug.LogError("[PersistentSingleton] Something went really wrong " +
+				               " - there should never be more than 1 singleton!" +
+				               " Reopenning the scene might fix it.");
+			}
+			else if (instance == null) {
+				instance = (T)FindObjectOfType(typeof(T));
+				
+				if (instance == null) {
+					GameObject container = new GameObject();
+					container.name = typeof(T)+"(PersistentSingleton)";
+					instance = (T)container.AddComponent(typeof(T));
+					DontDestroyOnLoad(instance.gameObject); //dont destroy instant to persist in every scene
+				}
+			}
+			return instance;
+		}
+	}
+}
