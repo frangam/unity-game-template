33a9741b5115caab3919a84a8a4b7ee9f2a44c61
Fran García <fmgarmo@gmail.com>
2014-12-14 18:14:59 +0100

    Initial Level Loader Approach

---
+++ Assets/GameTemplate/Scripts/Core/Levels/BaseLevel.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class BaseLevel {
+	//--------------------------------------
+	// Constants
+	//--------------------------------------
+	public const char 	SEPARATOR_ATTRIBUTES 		= ',';
+	public const int 	INITIAL_PLAYER_LIFE 		= 1000;
+	public const int 	INITIAL_GAME_LIFES			= 1;
+
+	//--------------------------------------
+	// Private Attributes
+	//--------------------------------------
+	private string id;
+
+	private int initialPlayerLife;
+	private int initialGameLifes;
+
+	private int playerLife;
+	private int gameLifes;
+	
+	//--------------------------------------
+	// Getters/Setters
+	//--------------------------------------
+	public string Id {
+		get {
+			return this.id;
+		}
+	}
+	public int InitialPlayerLife {
+		get {
+			return this.initialPlayerLife;
+		}
+	}
+	public int InitialGameLifes {
+		get {
+			return this.initialGameLifes;
+		}
+	}
+	public int PlayerLife {
+		get {
+			return this.playerLife;
+		}
+		set {
+			playerLife = value;
+		}
+	}
+	public int GameLifes {
+		get {
+			return this.gameLifes;
+		}
+		set {
+			gameLifes = value;
+		}
+	}
+	
+	//--------------------------------------
+	// Constructors
+	//--------------------------------------
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BaseLevel"/> class.
+	/// 
+	/// Attributes:
+	/// ID, Player life
+	/// </summary>
+	/// <param name="attributes">Attributes.</param>
+	public BaseLevel(string attributes){
+		string[] att = attributes.Split(SEPARATOR_ATTRIBUTES);
+
+		//id
+		if(att.Length > 0){
+			id = att[0];
+		}
+
+		//player life
+		int life;
+		if(att.Length > 1 && int.TryParse(att[1], out life)){
+			initialPlayerLife = life;
+		}
+		else{
+			initialPlayerLife = INITIAL_PLAYER_LIFE;
+		}
+
+		//game lifes
+		int gLifes;
+		if(att.Length > 2 && int.TryParse(att[2], out gLifes)){
+			initialGameLifes = gLifes;
+		}
+		else{
+			initialGameLifes = INITIAL_GAME_LIFES;
+		}
+	}
+
+	//--------------------------------------
+	// Overriden Methods
+	//--------------------------------------
+
+	//--------------------------------------
+	// Public Methods
+	//--------------------------------------
+	public virtual void reset(){
+		playerLife = initialPlayerLife;
+		gameLifes = initialGameLifes;
+	}
+}
---
+++ Assets/GameTemplate/Scripts/Core/Levels/BaseLevelLoaderController.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseLevelLoaderController : Singleton<BaseLevelLoaderController> {
+	//--------------------------------------
+	// Static Attributes
+	//--------------------------------------
+	
+	//--------------------------------------
+	// Constants
+	//--------------------------------------
+	public const char 	SEPARATOR_ATTRIBUTES 		= ',';
+	
+	//--------------------------------------
+	// Setting Attributes
+	//--------------------------------------
+	[SerializeField]
+	private string levelFile = "Levels";
+		
+	//--------------------------------------
+	// Private Attributes
+	//--------------------------------------
+	
+	//--------------------------------------
+	// Getters/Setters
+	//--------------------------------------
+	
+	//--------------------------------------
+	// Overriden Methods
+	//--------------------------------------
+	
+	//--------------------------------------
+	// Unity Methods
+	//--------------------------------------
+	#region Unity
+	public virtual void Awake(){
+		
+	}
+	#endregion
+	
+	//--------------------------------------
+	// Private Methods
+	//--------------------------------------
+	
+	//--------------------------------------
+	// Public Methods
+	//--------------------------------------
+	/// <summary>
+	/// Gets all of the content levels file, that is, all of the text in levels file
+	/// </summary>
+	/// <returns>The content level file.</returns>
+	public string getContentLevelFile(){
+		bool located = false;
+		string content = "";
+
+		//Try to load levels file
+		TextAsset text = Resources.Load(levelFile) as TextAsset;
+
+		if(text == null){
+			Debug.LogError("You must provide a correct Levels filename");
+		}
+		else{
+			located = true;
+		}
+		
+		if(located){
+			content = text.text;
+
+			if(string.IsNullOrEmpty(content)){
+				Debug.LogWarning("Levels filename is empty or is invalid");
+			}
+		}
+
+		return content;
+	}
+
+	/// <summary>
+	/// Gets the content (all attributes) of the current level selected
+	/// </summary>
+	/// <returns>The current level content.</returns>
+	public string getCurrentLevelContent(){
+		string content = getContentLevelFile();
+		string[] lines = null;
+		string result = "";
+
+		if(!string.IsNullOrEmpty(content)){
+			lines = content.Split('\n'); //get each line of the content
+
+			if(lines == null || lines.Length == 0){
+				Debug.LogWarning("Levels content is empty or is invalid");
+			}
+			else{
+				int currentLevel = PlayerPrefs.GetInt(GameSettings.PP_SELECTED_LEVEL); // get the current level selected
+
+				foreach(string line in lines){
+					string[] level = line.Split(SEPARATOR_ATTRIBUTES);
+					int levelId;
+
+					//first attribute is the level id
+					if(int.TryParse(level[0], out levelId) && levelId == currentLevel){
+						result = line;
+						break;
+					}
+				}
+			}
+		}
+
+		return result;
+	}
+
+	/// <summary>
+	/// Do start the level load
+	/// </summary>
+	public virtual void loadLevel(){
+
+	}
+	
+	//--------------------------------------
+	//  EVENTS
+	//--------------------------------------
+}
