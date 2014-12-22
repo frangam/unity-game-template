0cd939c1f09ee997eed2e033039751522045a26a
Fran García <fmgarmo@gmail.com>
2014-12-22 18:12:44 +0100

    Added a useful script close button

---
+++ Assets/GameTemplate/Scripts/UI/Button/UICloseButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UICloseButton : UIBaseButton {
+	public Image panel;
+
+	public override void press (){
+		base.press ();
+
+		panel.gameObject.SetActive(false);
+	}
+}
