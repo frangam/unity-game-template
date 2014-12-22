d6fa16540ef91e1d4480230312fa650a81fef2ac
Alfonso Cueto <obseiker@gmail.com>
2014-12-22 20:46:30 +0100

    rename PersistentSingleton.cs correct...

--- Assets/GameTemplate/Scripts/Common/Patterns/Singleton/PersistentSingleton.cs
+++ Assets/GameTemplate/Scripts/Common/Patterns/Singleton/PersistentSingleton.cs
@@ -4,7 +4,7 @@ using System.Collections;
 /// <summary>
 /// Singleton pattern for object we want to exist in all of the Scenes
 /// </summary>
-public abstract class PeristentSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+public abstract class PersistentSingleton<T> : MonoBehaviour where T : MonoBehaviour {
 	
 	private static T _instance = null;
 	private bool applicationIsQuitting = false;
@@ -22,7 +22,7 @@ public abstract class PeristentSingleton<T> : MonoBehaviour where T : MonoBehavi
 				_instance = GameObject.FindObjectOfType(typeof(T)) as T;
 				if (_instance == null) {
 					_instance = new GameObject ().AddComponent<T> ();
-					_instance.gameObject.name = _instance.GetType ().Name+" (PeristentSingleton)";
+					_instance.gameObject.name = _instance.GetType ().Name+" (PersistentSingleton)";
 				}
 			}
 			
