7834ae797ef76bb4d97fb912c4fd8005ed8def10
Alfonso Cueto <obseiker@gmail.com>
2014-12-22 20:57:31 +0100

    esta es la buena xD

--- Assets/GameTemplate/Scripts/Common/Patterns/Singleton/PersistentSingleton.cs
+++ Assets/GameTemplate/Scripts/Common/Patterns/Singleton/PersistentSingleton.cs
@@ -7,17 +7,12 @@ using System.Collections;
 public abstract class PersistentSingleton<T> : MonoBehaviour where T : MonoBehaviour {
 	
 	private static T _instance = null;
-	private bool applicationIsQuitting = false;
 	
 	
-	public static T instance {
+	
+	public static T Instance {
 		
 		get {
-			if(applicationIsQuitting) {
-				Debug.Log(typeof(T) + " [Mog.Singleton] is already destroyed. Returning null. Please check HasInstance first before accessing instance in destructor.");
-				return null;
-			}
-			
 			if (_instance == null) {
 				_instance = GameObject.FindObjectOfType(typeof(T)) as T;
 				if (_instance == null) {
@@ -30,6 +25,16 @@ public abstract class PersistentSingleton<T> : MonoBehaviour where T : MonoBehav
 			
 		}
 		
+	}
+	
+	public static bool IsDestroyed {
+		get {
+			if(_instance == null) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 
 	public virtual void Start(){
--- Assets/GameTemplate/Scripts/Common/Patterns/Singleton/Singleton.cs
+++ Assets/GameTemplate/Scripts/Common/Patterns/Singleton/Singleton.cs
@@ -7,22 +7,17 @@ using System.Collections;
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
 	
 	private static T _instance = null;
-	private bool applicationIsQuitting = false;
 	
 	
-	public static T instance {
+	
+	public static T Instance {
 		
 		get {
-			if(applicationIsQuitting) {
-				Debug.Log(typeof(T) + " [Mog.Singleton] is already destroyed. Returning null. Please check HasInstance first before accessing instance in destructor.");
-				return null;
-			}
-			
 			if (_instance == null) {
 				_instance = GameObject.FindObjectOfType(typeof(T)) as T;
 				if (_instance == null) {
 					_instance = new GameObject ().AddComponent<T> ();
-					_instance.gameObject.name = _instance.GetType ().Name+" (Singlenton)";
+					_instance.gameObject.name = _instance.GetType ().Name+" (Singleton)";
 				}
 			}
 			
@@ -31,11 +26,14 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
 		}
 		
 	}
-
-	protected virtual void OnApplicationQuit () {
-		Debug.Log("OnApplicationQuit");
-		_instance = null;
-		applicationIsQuitting = true;
-		Debug.Log(typeof(T) + " [Mog.Singleton] instance destroyed with the OnApplicationQuit event");
+	
+	public static bool IsDestroyed {
+		get {
+			if(_instance == null) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 }
