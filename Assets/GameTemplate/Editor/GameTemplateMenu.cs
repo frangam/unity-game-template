447dd30dff3110ecadc038c0b999bbbd1a796fef
Fran García <fmgarmo@gmail.com>
2015-01-09 17:01:30 +0100

    Added to Window/GameTemplate menu useful links like the Game Settings asset and Documentation links

---
+++ Assets/GameTemplate/Editor/GameTemplateMenu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class GameTemplateMenu : EditorWindow {
+	
+	//--------------------------------------
+	//  PUBLIC METHODS
+	//--------------------------------------
+	
+	#if UNITY_EDITOR
+	
+	[MenuItem("Window/Game Template/Game Settings", false, 0)]
+	public static void Edit() {
+		Selection.activeObject = GameSettings.Instance;
+	}
+
+	//--------------------------------------
+	//  Documentation
+	//--------------------------------------
+
+	[MenuItem("Window/Game Template/Documentation/All Docs", false, 0)]
+	public static void AllDocs() {
+		string url = "https://drive.google.com/drive/u/0/#folders/0B9SJ925Use_uT2hBUzhBcnBIbUE/0B9SJ925Use_uVlJQel93WHRzYU0/0B9SJ925Use_uSjlXZFU2d2ZfODQ";
+		Application.OpenURL(url);
+	}
+
+	//--------------------------------------
+	//  Achievemente Documentation
+	//--------------------------------------
+
+	[MenuItem("Window/Game Template/Documentation/Achievements/Getting Started")]
+	public static void AchievementSystemGettingStartedDoc() {
+		string url = "https://docs.google.com/document/d/17kOcOnMRrLfwEKrG9xs-shzGYEzBCSOTwJe8akRC0vo/edit#heading=h.ujroj168a0eg";
+		Application.OpenURL(url);
+	}
+
+	[MenuItem("Window/Game Template/Documentation/Achievements/Workflow")]
+	public static void AchievementSystemWorkflowDoc() {
+		string url = "https://docs.google.com/document/d/17kOcOnMRrLfwEKrG9xs-shzGYEzBCSOTwJe8akRC0vo/edit#heading=h.4rrfds5d75hd";
+		Application.OpenURL(url);
+	}
+
+	[MenuItem("Window/Game Template/Documentation/Achievements/Setup")]
+	public static void AchievementSystemSetupDoc() {
+		string url = "https://docs.google.com/document/d/17kOcOnMRrLfwEKrG9xs-shzGYEzBCSOTwJe8akRC0vo/edit#heading=h.wynqvbtoesnn";
+		Application.OpenURL(url);
+	}
+
+
+	
+	#endif
+	
+}
