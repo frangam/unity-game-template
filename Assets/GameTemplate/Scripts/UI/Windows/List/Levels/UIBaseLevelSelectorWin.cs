126d7135ce505bcb1915e249ce38cb2c2aa9e336
Fran García <fmgarmo@gmail.com>
2015-02-10 20:52:16 +0100

    improvements in levelselector win

--- Assets/GameTemplate/Scripts/UI/Windows/List/Levels/UIBaseLevelSelectorWin.cs
+++ Assets/GameTemplate/Scripts/UI/Windows/List/Levels/UIBaseLevelSelectorWin.cs
@@ -15,76 +15,112 @@ public class UIBaseLevelSelectorWin : UIBaseListWindow {
 	
 	[SerializeField]
 	private Text lbGemsReward;
-
+	
 	[SerializeField]
 	private Transform pnlGemsReward;
-
-
+	
+	[SerializeField]
+	private UIBasePlayLevelButton pbLevelButton;
+	
+	[SerializeField]
+	private Transform populateWithLevelButtonPb;
+	
+	[SerializeField]
+	private List<UIBasePlayLevelButton> levelButtons;
+	
 	[SerializeField]
 	private string sceneToGoAtSelectLevel = GameSettings.SCENE_GAME;
-
+	
 	//--------------------------------------
 	// Private Attributes
 	//--------------------------------------
 	private int 			lastUnlockedLevel;
-
+	
 	//--------------------------------------
 	// Overriden Methods
 	//--------------------------------------
 	public override void loadItems ()
 	{
 		base.loadItems ();
-
+		
 		if(indexCurrentItem >= items.Count)
 			indexCurrentItem--;
+		
+		loadLevelButtons();
 	}
-
+	
 	public override UIBaseListItem createListItem (string data)
 	{
 		return new UIBaseLevel(data);
 	}
-
+	
 	public override void setLabelName (UIBaseListItem item)
 	{
 		if(lbNameCurrentItem){
 			UIBaseLevel level = (UIBaseLevel) item;
-
+			
 			if(level != null)
 				lbNameCurrentItem.text = level.Id+"/"+items.Count.ToString();
 		}
 	}
-
+	
 	public override void initIndexCurrentItem ()
 	{
 		int last = PlayerPrefs.GetInt(GameSettings.PP_LAST_LEVEL_UNLOCKED);
-
+		
 		if(last == 0)
 			PlayerPrefs.SetInt(GameSettings.PP_LAST_LEVEL_UNLOCKED, 1);
-
+		
 		lastUnlockedLevel = last == 0 ? 1: last; 
 		indexCurrentItem = lastUnlockedLevel - 1; //-1 porque el primer nivel es el 1 y el indice debe empezar en 0;
 	}
-
+	
 	public override void initIndexLastItem ()
 	{
 		indexLastItem = lastUnlockedLevel;
 	}
-
+	
 	public override void showItem (UIBaseListItem item)
 	{
 		base.showItem (item);
-
+		
 		UIBaseLevel uiLevel = (UIBaseLevel) item;
 		
 		if(uiLevel != null && uiLevel.Level != null)
 			showLevel(uiLevel.Level);
 	}
-
+	
+	
 	//--------------------------------------
 	// Public Methods
 	//--------------------------------------
-
-
+	public void loadLevelButtons(){
+		if((levelButtons == null || (levelButtons != null && levelButtons.Count == 0)) && pbLevelButton && populateWithLevelButtonPb){
+			loadLevelForButtons();
+		}
+	}
+	
+	public virtual void loadLevelForButtons(){
+		
+		
+		if(items != null && items.Count > 0){
+			levelButtons = new List<UIBasePlayLevelButton>();
+			
+			foreach(UIBaseListItem item in items){
+				UIBaseLevel uiLevel = (UIBaseLevel) item;
+				
+				if(uiLevel.Level != null){
+					UIBasePlayLevelButton button = pbLevelButton.Spawn();
+					button.transform.parent = populateWithLevelButtonPb;
+					button.init(uiLevel.Level, this);
+					levelButtons.Add(button);
+				}
+			}
+		}
+	}
+	
+	
+	
 	public virtual void showLevel(BaseLevel level){
 		if(lbLevelDesc)
 			lbLevelDesc.text = level.LocalizedDescription;
@@ -92,22 +128,32 @@ public class UIBaseLevelSelectorWin : UIBaseListWindow {
 			lbMoneyReward.text = level.RealMoneyReward.ToString();
 		if(lbGemsReward)
 			lbGemsReward.text = level.RealGemsReward.ToString();
-
+		
 		if(pnlGemsReward)
 			pnlGemsReward.gameObject.SetActive(level.RealGemsReward > 0);
 	}
 	
-	public virtual void playLevel(){
-		int l;
+	public virtual void playLevel(string levelId = ""){
+		string _id = items[indexCurrentItem].Id; //id from the current item selected
 		
-		if(int.TryParse(items[indexCurrentItem].Id, out l)){
-			PlayerPrefs.SetInt(GameSettings.PP_SELECTED_LEVEL, l);
+		//the given id
+		if(!string.IsNullOrEmpty(levelId)){
+			_id = levelId;
+		}
+		
+		//select and play level
+		if(!string.IsNullOrEmpty(_id)){
+			int l;
+			
+			if(int.TryParse(_id, out l)){
+				PlayerPrefs.SetInt(GameSettings.PP_SELECTED_LEVEL, l);
 				ScreenLoaderVisualIndicator.Instance.LoadScene(sceneToGoAtSelectLevel);
+			}
 		}
 	}
-
+	
 	public virtual void showMissions(bool show){
 		int pShow = show ? 1: 0;
-			PlayerPrefs.SetInt(GameSettings.PP_SHOW_MISSIONS_WINDOW, pShow);
+		PlayerPrefs.SetInt(GameSettings.PP_SHOW_MISSIONS_WINDOW, pShow);
 	}
 }
--- Assets/GameTemplate/Scripts/UI/Windows/List/Levels/UIBasePlayLevelButton.cs
+++ Assets/GameTemplate/Scripts/UI/Windows/List/Levels/UIBasePlayLevelButton.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UIBasePlayLevelButton : UIBaseButton {
@@ -7,24 +8,74 @@ public class UIBasePlayLevelButton : UIBaseButton {
 	//--------------------------------------
 	[SerializeField]
 	private UIBaseLevelSelectorWin window;
-
+	
+	[SerializeField]
+	private Text lbLevelID;
+	
+	[SerializeField]
+	private Image spLock;
+	
+	//--------------------------------------
+	// Private Attributes
+	//--------------------------------------
+	private BaseLevel level;
+	private bool unlocked = true;
+	
 	//--------------------------------------
 	// Overriden Methods
 	//--------------------------------------
 	public override void Awake ()
 	{
 		base.Awake ();
-
+		
 		if(window == null)
 			window = GetComponentInParent<UIBaseLevelSelectorWin>();
 	}
+	
+	protected override bool canPress ()
+	{
+		return unlocked;
+	}
+	
 	protected override void doPress ()
 	{
 		base.doPress ();
-
-		if(window != null)
-			window.playLevel();
+		
+		if(window != null){
+			if(level != null)
+				window.playLevel(level.Id);
+			else
+				window.playLevel();
+		}
 		else
 			Debug.LogError("Not found UIBaseLevelSelectorWin");
+	}
+	
+	//--------------------------------------
+	// Public Methods
+	//--------------------------------------
+	public void init(BaseLevel level, UIBaseLevelSelectorWin _window){
+		if(level != null){
+			this.level = level;
+			
+			if(lbLevelID)
+				lbLevelID.text = level.Id;
+			
+			if(spLock){
+				int lastUnlockedLevel = PlayerPrefs.GetInt(GameSettings.PP_LAST_LEVEL_UNLOCKED);
+				int id;
+				
+				
+				if(int.TryParse(level.Id, out id)){
+					unlocked = id <= lastUnlockedLevel;
+					button.interactable = unlocked;
+					spLock.gameObject.SetActive(!unlocked);
+				}
+			}
+			
+			
+			
+			this.window = _window;
+		}
 	}
 }
