272d165a9aa74915f24629de34c2a7b85009a1c9
Fran García <fmgarmo@gmail.com>
2015-07-02 14:29:26 +0200

    added a button to show an ad when it is pressed

---
+++ Assets/GameTemplate/Scripts/Core/NetworkServices/Advertising/AdType.cs
@@ -0,0 +1,8 @@
+public enum AdType{
+	ALL 			= -3,
+	SOME			= -2,
+	NONE			= -1,
+	INTERSTITIAL 	= 0,
+	VIDEO			= 1,
+	BANNER			= 2
+}
---
+++ Assets/GameTemplate/Scripts/UI/Button/BtnShowAd.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnShowAd : UIBaseButton {
+	//--------------------------------------
+	// Setting Attributes
+	//--------------------------------------
+	[SerializeField]
+	private AdType adType = AdType.INTERSTITIAL; //the ad type to show
+
+	//--------------------------------------
+	// Overriden Methods
+	//--------------------------------------
+	protected override void doPress ()
+	{
+		base.doPress ();
+
+		switch(adType){
+		case AdType.INTERSTITIAL: AdsHandler.Instance.mostrarPantallazo(); break;
+		}
+	}
+
+	protected override bool canPress ()
+	{
+		return !GameSettings.Instance.IS_PRO_VERSION && base.canPress ();
+	}
+}
